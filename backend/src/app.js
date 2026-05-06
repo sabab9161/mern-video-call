87ce87adcb8bc11183f809697c8bac8f73dbea37
sabab9161 <sababhussain107@gmail.com>
2026-05-06 17:25:57 +0530

    Fix dotenv import

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
-import cors from "cors";
+import cors  from "cors";
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
