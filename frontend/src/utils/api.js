e5bdbab465c9ceb470a95f4d32685224b8931101
sabab9161 <sababhussain107@gmail.com>
2026-05-08 20:01:21 +0530

    Fixed API base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://mern-video-call-13.onrender.com",
+  withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
