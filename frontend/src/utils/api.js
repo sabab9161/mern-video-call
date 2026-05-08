de77fe19756235a5d8d8d7c4aff572855e1c6fd6
sabab9161 <sababhussain107@gmail.com>
2026-05-08 17:13:09 +0530

    Fix production API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5000/api";
-export const API_ORIGIN = API_BASE_URL.replace(/\/api\/?$/, "");
-
 const api = axios.create({
-  baseURL: API_BASE_URL
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
 });
 
 api.interceptors.request.use((config) => {
@@ -14,25 +13,11 @@ api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
-  // IMPORTANT: let browser set multipart boundary automatically
   if (config.data instanceof FormData) {
     delete config.headers["Content-Type"];
   }
 
   return config;
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("user");
-      window.dispatchEvent(new Event("auth:unauthorized"));
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default api;
