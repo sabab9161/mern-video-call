e87dfc14803a43d5c49a79a3b2147d1f864f7ef1
sabab9161 <sababhussain107@gmail.com>
2026-05-08 17:21:12 +0530

    Fix API_ORIGIN export

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+export const API_ORIGIN =
+  import.meta.env.VITE_SOCKET_URL ||
+  "http://localhost:5000";
+
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
